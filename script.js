ce14e1fa802cc6269e05d68e44c21eb3582ed8c2
adamkatsu <adamkatsu369@gmail.com>
2024-11-25 15:55:36 +0700

    feat: leaflet js

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ new svgMap({
     }
   }
 });
+
+var map = L.map('map').setView([0, 0], 2);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 5,
+  minZoom: 2,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
