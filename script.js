f7b4d4a754f98601f5e8afb435e80a9be9300c1c
adamkatsu <adamkatsu369@gmail.com>
2024-11-26 14:39:07 +0700

    feat: list selected countries

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // GeoJSON URL (replace this with your own or a hosted version)
 const countriesGeoJSON = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json';
 
+// Array to hold selected countries
+const selectedCountries = [];
+
 // Fetch and add GeoJSON layer
 fetch(countriesGeoJSON)
   .then(response => response.json())
@@ -30,7 +33,7 @@ fetch(countriesGeoJSON)
               layer.setStyle({ fillOpacity: 0.7 });
           }
           const countryName = feature.properties.name; // Assuming `name` property exists
-          const countryData = `Data about ${countryName}`; // Replace with actual data source
+          const countryData = `Hello ${countryName}`; // Replace with actual data source
           L.popup()
               .setLatLng(layer.getBounds().getCenter())
               .setContent(`<b>${countryName}</b><br>${countryData}`)
@@ -43,11 +46,23 @@ fetch(countriesGeoJSON)
         });
         // Toggle color on click
         layer.on('click', function () {
+          const countryName = feature.properties.name;
           isSelected = !isSelected;
           if (isSelected) {
               layer.setStyle({ fillColor: 'red' }); // Change to red
+              selectedCountries.push(countryName); // Add to array
           } else {
               layer.setStyle({ fillColor: '#3388ff' }); // Revert to original
+              // Remove from array
+              const index = selectedCountries.indexOf(countryName);
+              if (index > -1) selectedCountries.splice(index, 1);
+          }
+          // Log the array (or handle it as needed)
+          console.log('Selected Countries:', selectedCountries);
+
+          document.querySelector('.countries-list').innerHTML = '';
+          for (const item of selectedCountries) {
+            document.querySelector('.countries-list').innerHTML += `<li>${item}</li>`
           }
         });
       }
