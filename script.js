fe9b4945932cdaa8ac1578a5ea71952d027949b6
adamkatsu <adamkatsu369@gmail.com>
2024-11-26 14:12:13 +0700

    feat: select country function

--- script.js
+++ script.js
@@ -1,32 +1,56 @@
-new svgMap({
-  targetElementID: 'svgMap',
-  data: {
-    data: {
-      gdp: {
-        name: 'GDP per capita',
-        format: '{0} USD',
-        thousandSeparator: ',',
-        thresholdMax: 50000,
-        thresholdMin: 1000
-      },
-      change: {
-        name: 'Change to year before',
-        format: '{0} %'
-      }
-    },
-    applyData: 'gdp',
-    values: {
-      AF: { gdp: 587, change: 4.73 },
-      AL: { gdp: 4583, change: 11.09 },
-      DZ: { gdp: 4293, change: 10.01 }
-      // ...
-    }
-  }
-});
-
 var map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 5,
   minZoom: 2,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+
+// GeoJSON URL (replace this with your own or a hosted version)
+const countriesGeoJSON = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json';
+
+// Fetch and add GeoJSON layer
+fetch(countriesGeoJSON)
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: {
+        color: 'grey',
+        weight: 1,
+        fillColor: '#3388ff',
+        fillOpacity: 0.3,
+      },
+      onEachFeature: function (feature, layer) {
+        // Track whether a country is "selected"
+        let isSelected = false;
+
+        // Highlight on hover
+        layer.on('mouseover', function () {
+          if (!isSelected) {
+              layer.setStyle({ fillOpacity: 0.7 });
+          }
+          const countryName = feature.properties.name; // Assuming `name` property exists
+          const countryData = `Data about ${countryName}`; // Replace with actual data source
+          L.popup()
+              .setLatLng(layer.getBounds().getCenter())
+              .setContent(`<b>${countryName}</b><br>${countryData}`)
+              .openOn(map);
+        });
+        layer.on('mouseout', function () {
+          if (!isSelected) {
+              layer.setStyle({ fillOpacity: 0.3 });
+          }
+        });
+        // Toggle color on click
+        layer.on('click', function () {
+          isSelected = !isSelected;
+          if (isSelected) {
+              layer.setStyle({ fillColor: 'red' }); // Change to red
+          } else {
+              layer.setStyle({ fillColor: '#3388ff' }); // Revert to original
+          }
+        });
+      }
+    }).addTo(map);
+})
+.catch(error => console.error('Error loading GeoJSON:', error));
